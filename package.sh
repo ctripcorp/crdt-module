cd48dc81ff0109ff23973f044f7028b6b5f4e769
NickNYU <cz739@nyu.edu>
2019-06-19 19:54:20 +0800

    add package.sh

---
+++ package.sh
@@ -0,0 +1,14 @@
+APP=crdt-module
+Version=1.0.0
+Package=$APP-$Version
+
+if [ -f $package ]; then
+    echo remove $Package
+    rm -rf $Package
+fi
+echo make clean
+mkdir $Package
+ls -a | egrep -v "Debug|^\.|$APP" | xargs -J %  cp -r %  $Package
+echo create $Package.tar.gz...
+tar -czf $Package.tar.gz $Package
+rm -rf $Package
